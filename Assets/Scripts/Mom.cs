bd99cf483452462eb7d5d2ea564e1c35efe9453e
amberkhatib2028 <ak1225@princeton.edu>
2026-09-07 14:08:13 +0200

    mom script just with states

---
+++ Assets/Scripts/Mom.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Mom : MonoBehaviour
+{
+    // carries mom's current state
+    private State MomState;
+
+    // options for state of mom
+    public enum State
+    {
+        Frosting,
+        Thinking,
+        Distracted
+    }
+
+    // state: mom is frosting cake
+    private void Frosting()
+    {
+        MomState = State.Frosting;
+    }
+
+    // state: mom is thinking about a distraction
+    private void Thinking()
+    {
+        MomState = State.Thinking;
+    }
+
+    // state: mom is distracted
+    private void Distracted()
+    {
+        MomState = State.Distracted;
+    }
+}
