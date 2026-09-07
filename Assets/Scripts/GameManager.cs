69f1514d3d7917d88e4aa11225440d28de11f957
SugarCube03 <69729029+SugarCube03@users.noreply.github.com>
2026-09-07 15:33:22 +0200

    working on gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private InputAction lickingKey;
+    private bool lickingTime;
+    void Awake()
     {
-        
+        lickingKey = InputSystem.actions.FindAction("Jump");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lickingTime = lickingKey.IsPressed();
+        print(lickingTime);
     }
 }
